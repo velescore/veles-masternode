c52c1c492a4bc1a8d85510ebcad2242f260700de
AltcoinBaggins <marian@priv.profiweb.biz>
2019-12-06 02:30:30 +0100

    VPN as dapp refactor

---
+++ app/dapps/vpn/gateway.py
@@ -0,0 +1,50 @@
+from openvpn_api import vpn
+from hashlib import blake2b
+from dapps.vpn import gateway
+
+class VPNManagementGateway(object):
+	salt = ''
+
+	def __init__(self, config, logger):
+		self.host = config['dapp.vpn'].get('host', '127.0.0.1')
+		self.port = config['dapp.vpn'].get('port', '21432')
+		self.config = config
+		self.logger = logger
+		self.openvpn = None
+
+	def get_status(self):
+		state = self.server().state
+		status = self.server().get_status()
+		rel = self.server().release
+		h1 = blake2b(digest_size=15)
+		h2 = blake2b(digest_size=30)
+		result = {
+			'client_list': [],
+			'up_since': str(state.up_since),
+			'state_name': state.state_name,
+			'client_count': len(status.client_list),
+			'server_options': rel[rel.find('['):rel.rfind(']')+1],
+			'server_version': self.server().version
+			}
+
+		for addr, client in status.client_list.items():
+			h1.update(addr.encode('utf-8'))
+			h2.update(client.common_name.encode('utf-8'))
+
+			result['client_list'] += [{
+				'session_id': h1.hexdigest(),
+				'client_id': h2.hexdigest(),
+				'bytes_received': int(client.bytes_received),
+				'bytes_sent': int(client.bytes_sent),
+				'connected_since': str(client.connected_since),
+				'payment_status': 'ALPHA_TRIAL'
+				}]
+
+		return result
+
+	def server(self):
+		if not self.openvpn:
+			self.openvpn = vpn.VPN(self.host, self.port)
+			self.openvpn.connect()
+
+		return self.openvpn
--- app/dapps/vpn/provider.py
+++ app/dapps/vpn/provider.py
@@ -2,19 +2,40 @@
 
 from dapps.vpn import facade
 from dapps.vpn import status
+from dapps.vpn import gateway
 
 class dAppProvider(object):
 	dapp_name = 'VPN'
+	dependencies = {}
 
 	def name(self):
 		return self.dapp_name
 
+	def register(self, name, dependency):
+		if not name in self.dependencies:
+			self.dependencies[name] = dependency
+
+	def singleton(self, name):
+		if name in self.dependencies:
+			return self.dependencies[name]
+		return None
+
 	def make_facade(self, container):
+		# Lazy load when needed first time
+		self.register('VPNManagementGateway', gateway.VPNManagementGateway(
+				config=container.app_config(), 
+				logger=container.logger(),
+				))
+		self.register('VPNStatusService', status.VPNStatusService(
+				config=container.app_config(), 
+				logger=container.logger(),
+				vpn_gateway=self.singleton('VPNManagementGateway'),
+				))
+
+		# Return main dApp facade that will act as API between other
+		# parts of system and internal services registered here.
 		return facade.VPNdAppFacade(
 			config=container.app_config(), 
 			logger=container.logger(),
-			status_service=status.VPNStatusService(
-				config=container.app_config(), 
-				logger=container.logger(),
-				)
+			status_service=self.singleton('VPNStatusService')
 			)
