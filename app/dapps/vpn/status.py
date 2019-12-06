e5ed85d05b43e654bfe97b7337c448c1f605e2b2
AltcoinBaggins <marian@priv.profiweb.biz>
2019-12-06 02:30:30 +0100

    VPN as dapp refactor

--- app/dapps/vpn/status.py
+++ app/dapps/vpn/status.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
 class VPNStatusService(object):
-	def __init__(self, config, logger):
+	def __init__(self, config, logger, vpn_gateway):
 		self.config = config
 		self.logger = logger
+		self.vpn_gateway = vpn_gateway
 
 	def service_status(self):
-		return {'coolness': 65535}
+		return {**self.vpn_gateway.get_status(), 'network_usage': self.get_usage()}
 
+	def get_usage():
+		bytes_in, bytes_out = self._get_network_bytes(self.config['dapps.vpn'].get('tun_interface', 'tun1'))
+		return { 'bytes_in': bytes_in, 'bytes_out': bytes_out}
+
+	def _get_network_bytes(interface):
+		for line in open('/proc/net/dev', 'r'):
+			if interface in line:
+				data = line.split('%s:' % interface)[1].split()
+				rx_bytes, tx_bytes = (data[0], data[8])
+				return (int(rx_bytes), int(tx_bytes))
