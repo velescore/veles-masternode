37849e5b41c7076bd1bcdd4992c4ab6d1d211a25
AltcoinBaggins <marian@priv.profiweb.biz>
2019-12-10 21:24:12 +0100

    Refactor Masternode list, models and model collection

--- app/masternode/interfaces.py
+++ app/masternode/interfaces.py
@@ -1,7 +1,9 @@
 """ Veles Masternode (gen 2) masternode module base classes """
 from abc import ABCMeta, abstractmethod
+import copy
 
 class AbstractFillableObject(object, metaclass=ABCMeta):
+	""" Object that can be easily fileld with attributes at creation """
 	_required = []
 
 	def __init__(self, attributes):
@@ -17,15 +19,38 @@ class AbstractFillableObject(object, metaclass=ABCMeta):
 		# Fill optional attributes (need to be declared with default value)
 		for name, value in attributes.items():
 			if hasattr(self, name):
-				setattr(self, name, value)
+				setattr(self, name, copy.copy(value))
 
 	def attributes(self):
 		attributes = {}
 
-		# Skip 'private' properties
+		# Put properties in dict, skip 'private' ones
 		for attr_name, attr_value in self.__dict__.items():
 			if attr_name[0] != '_':
 				attributes[attr_name] = attr_value
+
+		return attributes
+
+class AbstractFillableCollection(dict, metaclass=ABCMeta):
+	""" Represents a dictionary of AbstractFillableObject instances """
+	def __init__(self, entries):
+		self.fill(entries)
+
+	def __setitem__(self, key, value):
+		#assert issubclass(value, AbstractFillableObject)
+		dict.__setitem__(self, key, value)
+
+	def fill(self, entries):
+		for key, value in entries.items():
+			self[key] = value
+
+	def attributes(self):
+		attributes = {}
+
+		# Get attributes of each item
+		for attr_name, attr_value in self.items():
+			if attr_name[0] != '_':
+				attributes[attr_name] = attr_value.attributes()
 
 		return attributes
 
--- app/masternode/masternode.py
+++ app/masternode/masternode.py
@@ -1,57 +1,89 @@
 """ Veles Masternode (gen 2) masternode models """
-from masternode.interfaces import AbstractFillableObject
+from masternode.interfaces import AbstractFillableObject, AbstractFillableCollection
 import copy
 
 class Masternode(AbstractFillableObject):
 	""" Represents a masternode """
 	_required = ['ip', 'outpoint', 'payee']		# Required attributes
+	status = 'UNKNOWN'
 	dapp_status = 'UNKNOWN'
-	last_checked = None
 	signing_key = None
+	last_checked = None
 
+class MasternodeCollection(AbstractFillableCollection):
+	""" Dictionary that holds Masternode objects """
+	def add(self, mn):
+		self[mn.ip] = mn
 
-class CoreInfoMixin(object):
-	""" Adds core daemon information about the masternode """
-	_core_info = {}
+class MasternodeList(MasternodeCollection):
+	""" Holds a masternode list, that can be obtained in several modes """
+	def __init__(self, entries, mode = None):
+		super().__init__(entries)
+		self._mode = mode
+
+	def get_mode(self):
+		return self._mode
+
+class MasternodeInfoList(MasternodeList):
+	def attributes(self, nested=True):
+		result = {}
+
+		for ip, mn in self.items():
+			result[ip] = mn.attributes(nested)
+
+		return result
+
+class MasternodeInfoMixin(Masternode):
+	""" Represents a last known state of a masternode with associated information """
 
 	def update_core_info(self, core_info):
-		self._core_info.update(core_info)
-
-	def get_core_info(self):
-		return self._core_info
-
-	def attributes(self):
-		return {**super().attributes(), **self._core_info}
-
-
-class VersionInfoMixin(object):
-	""" Adds version information about the masternode """
-	_version_info = {}
+		self._core_info = core_info
 
 	def update_version_info(self, version_info):
-		self._version_info.update(version_info)
-
-	def get_version_info(self):
-		return self._version_info
-
-	def attributes(self):
-		return {**super().attributes(), **self._version_info}
-
-
-class ServiceInfoMixin(object):
-	""" Adds service information about the masternode """
-	_service_info = {}
+		self._version_info = version_info
 
 	def update_service_info(self, service_info):
-		self._service_info.update(service_info)
+		self._service_info = service_info
+
+	def get_core_info(self):
+		if not hasattr(self, '_core_info'):
+			return {}
+
+		return self._core_info
 
 	def get_service_info(self):
+		if not hasattr(self, '_service_info'):
+			return {}
+
 		return self._service_info
 
-	def attributes(self):
-		return {**super().attributes(), **self._service_info}
+	def get_version_info(self):
+		if not hasattr(self, '_version_info'):
+			return {}
 
+		return self._version_info
 
-class MasternodeInfo(ServiceInfoMixin, VersionInfoMixin, CoreInfoMixin, Masternode):
+	def attributes(self, nested=False):
+		result = super().attributes()
+
+		if nested:
+			if self.get_core_info():
+				result['core'] = self.get_core_info()
+			if self.get_service_info():
+				result['services'] = self.get_service_info()
+			if self.get_version_info():
+				result['version'] = self.get_version_info()
+		else:
+			result = {
+				**result,
+				**self.get_core_info(),
+				**self.get_version_info(),
+				**self.get_service_info(),
+			}
+
+		return result
+
+class MasternodeInfo(MasternodeInfoMixin, Masternode):
 	""" Represents a last known state of a masternode with associated information """
 	pass
+
