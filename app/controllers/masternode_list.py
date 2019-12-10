fe6c17fa5e3ffba384b84094796d371c0eec2d5a
AltcoinBaggins <marian@priv.profiweb.biz>
2019-12-10 20:10:02 +0100

    Implement new version of masternode list

--- app/controllers/masternode_list.py
+++ app/controllers/masternode_list.py
@@ -12,6 +12,15 @@ class MasternodeListController(AbstractSigningController):
 		try:
 			mn_list = self.mnsync_service.get_masternode_list()
 		except Exception as e:
+			self.logger.error('Failed to obtain masternode list: ' + str(e))
 			return self.error_response('Failed to obtain masternode list', 559)	# first 4 letters of "wallet" on T9 keyboard
 
-		return self.response(mn_list)
+		return self.response(self._normalize(mn_list))
+
+	def _normalize(self, mn_list):
+		result = {}
+
+		for ip, mn in mn_list.items():
+			result[ip] = mn.attributes()
+
+		return result
