917b6cf6787ea8c5e75cf04a253cdc52fd47ff22
AltcoinBaggins <marian@priv.profiweb.biz>
2020-03-23 00:12:48 +0100

    New installation method as native Python package

--- data/install/pre_install.sh
+++ data/install/pre_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 do_pre_install() {
-	# Cleaning up
+  # Cleaning up. Could be removed few versions later after 1.99.05, 
+  # but for now always remove the files of the old version
 	rm -r ${DIST_PREFIX}/usr/lib/veles/masternode
 }
