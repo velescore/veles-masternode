028b62f63cca7ab9f58f5c5b9017a86dbd58e4af
AltcoinBaggins <marian@priv.profiweb.biz>
2020-03-25 17:11:30 +0100

    Pass local source path to the installator

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Fetches the installator wizard and performs full Veles Masternode
+# installation from this source directory
 
 echo "Preparing the installation environment ..."
 git clone https://github.com/AltcoinBaggins/masternode-installer2 /tmp/veles-masternode-installer
-(cd /tmp/veles-masternode-installer && make install)
+(export VELES_MN_SRC=$(dirname $0) ; cd /tmp/veles-masternode-installer && make install)
 rm -rf /tmp/veles-masternode-installer
